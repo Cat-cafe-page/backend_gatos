7e3844a00790e49b5f2c173bb6462d03c17cda83
daniela904 <daniela.med51@gmail.com>
2022-01-27 16:58:30 -0500

    Spring first configuration

---
+++ src/main/java/com/catcafe/backend_gatos/BackendGatosApplication.java
@@ -0,0 +1,13 @@
+package com.catcafe.backend_gatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGatosApplication.class, args);
+	}
+
+}
